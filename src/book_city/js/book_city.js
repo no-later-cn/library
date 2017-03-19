64a9359d43ff5e6d3ce9dca95523ae55efa2e621
GreatPresonal <405454518@163.com>
2017-03-20 00:26:55 +0800

    传6页

---
+++ src/book_city/js/book_city.js
@@ -0,0 +1,11 @@
+$(function () {
+    var selector=$(".blue");
+    var next=$(".next");
+    selector.on('click',function () {
+        selector.removeClass('active');
+        $(this).addClass('active');
+       var i=$(this).index();
+       next.removeClass('next');
+       $(next).eq(i).addClass('next');
+    })
+})
