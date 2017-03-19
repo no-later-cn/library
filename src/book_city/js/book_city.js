462116d7d492cca77a3fc7d90ee34aa45881b283
GreatPresonal <405454518@163.com>
2017-03-20 00:43:55 +0800

    传6页

--- src/book_city/js/book_city.js
+++ src/book_city/js/book_city.js
@@ -5,7 +5,6 @@ $(function () {
         selector.removeClass('active');
         $(this).addClass('active');
        var i=$(this).index();
-       next.removeClass('next');
-       $(next).eq(i).addClass('next');
+       $(next).removeClass('nexts').eq(i).addClass('nexts');
     })
 })
